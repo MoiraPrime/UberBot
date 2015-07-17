73b5c4f2cc4c15b3e1211f28484e979bcc9a46f6
UberActivist <UberActivist@gmail.com>
2015-07-16 23:22:52 -0500

    fixes

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 //Required for Openshift
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-
-server.listen(server_port, server_ip_address, function(){
-  console.log("Listening on " + server_ip_address + ", server_port " + server_port)
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + port )
 });
 
 // Create the configuration
