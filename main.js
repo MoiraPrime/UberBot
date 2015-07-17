1ab900ba35470280195649275e57763154460ce7
UberActivist <UberActivist@gmail.com>
2015-07-16 23:39:07 -0500

    maybe this will work

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 //Required for Openshift
+var http = require('http');
+
+function handleRequest(request, response){
+    response.end('It Works!! Path Hit: ' + request.url);
+}
+
+var server = http.createServer(handleRequest);
+
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
  
